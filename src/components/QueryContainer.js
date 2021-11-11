ec1b342ef1b92f752ec0dde4c46d8e3187fb0af1
Valentin Grouès <valentin.groues@uni.lu>
2021-11-11 11:43:42 +0100

    add disease query box

--- src/components/QueryContainer.js
+++ src/components/QueryContainer.js
@@ -1,6 +1,7 @@
 import './QueryContainer.css';
 import clipboard from '../images/clipboard.svg';
 import dash from '../images/dash.svg';
+import syringe from '../images/syringe.svg';
 
 function QueryBoxMeasurement() {
     return <form className={"queryBox"}>
@@ -58,12 +59,64 @@ function QueryBoxValue() {
     </form>;
 }
 
+function QueryBoxDisease() {
+    return <form className={"queryBox"}>
+        <img className={"queryBox__icon"} src={syringe} alt={"dash-icon"}/>
+        <h3 className={"queryBox__title"}>For a specific disease and onset age</h3>
+        <div className={"inputGroup"}>
+
+            <label>disease</label>
+            <select className={"inputGroup__select"}>
+                <option>COVID-19</option>
+            </select>
+        </div>
+        <div className={"inputGroup"}>
+
+            <label>measurement</label>
+            <select className={"inputGroup__select"}>
+                <option>Gender</option>
+            </select>
+        </div>
+        <div className={"inputGroup"}>
+
+            <label>value</label>
+            <select className={"inputGroup__select"}>
+                <option>Female</option>
+                <option>Male</option>
+                <option>Other</option>
+            </select>
+        </div>
+        <div className={"inputGroup"}>
+            <label>onset age range</label>
+            <div className={"inputGroup__inline"}>
+                <input className={"inputGroup__inputSmall"} type={"number"}/>
+                <span>to</span>
+                <input className={"inputGroup__inputSmall"} type={"number"}/>
+            </div>
+        </div>
+        <div className={"inputGroup"}>
+            <p>
+                <label className={"inputGroup__labelCheckBox"}>include descendants
+                    <input className={"inputGroup__checkbox"} type={"checkbox"}/>
+                    <span className={"inputGroup__checkmark"}></span>
+                </label>
+            </p>
+        </div>
+
+        <div className={"inputSubmitButtonGroup"}>
+            <input className={"button"} type={"submit"} value={"search"}/>
+        </div>
+
+    </form>;
+}
+
 export function QueryContainer() {
     return <main className={"queryContainer"}>
         <h2 className={"queryContainer__title"}>Search Dataset</h2>
         <div className={"queryBoxGrid"}>
             <QueryBoxMeasurement/>
             <QueryBoxValue/>
+            <QueryBoxDisease/>
         </div>
     </main>;
 }
